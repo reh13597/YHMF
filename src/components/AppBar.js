3b1cd406758e3c0d1b6877478f62f666f36c13be
Alex Guo <alexlguo18@gmail.com>
2022-08-02 12:16:31 -0700

    AppBar buttons can now be highlighted upon being clicked (colors need to be tweaked)

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,17 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 export default function AppBarItems() {
+  const [btnColor, setBtnColor] = React.useState('#7F95D1');
+
+  const highlightBtn = (event, newBtnColor) => {
+    setBtnColor(newBtnColor);
+  };
+
   return (
     <Box sx={{flexGrow: 1,}}>
       <AppBar id='appBar' className='appBar' position="static" >
         <Toolbar>
+
           {/* <Router>
             <SideBar/>
             <Routes>
@@ -30,11 +38,82 @@ export default function AppBarItems() {
           
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <Button variant='text' style={{color: 'white', backgroundColor: '#5A678E', letterSpacing: 2}}>
-              Home</Button>
-            <Button variant='text' style={{color: 'white', letterSpacing: 2}}>The Team</Button>
-            <Button variant='text' style={{color: 'white', letterSpacing: 2}}>Events</Button>
-            <Button variant='text' style={{color: 'white', letterSpacing: 2}}>Contact</Button>
+          <ToggleButtonGroup
+            value={btnColor}
+            exclusive
+            onChange={highlightBtn}
+          >
+            <ToggleButton value="left" aria-label="left aligned">
+              <Typography
+                variant="h7"
+                noWrap
+                component="div"
+                sx={{ display: { xs: 'none', sm: 'block' } }}
+                style={{color: 'white', letterSpacing: 3}}
+              >
+                Home
+              </Typography>
+            </ToggleButton>
+            <ToggleButton value="center" aria-label="centered">
+              <Typography
+                variant="h7"
+                noWrap
+                component="div"
+                sx={{ display: { xs: 'none', sm: 'block' } }}
+                style={{color: 'white', letterSpacing: 3}}
+              >
+                The Team
+              </Typography>
+            </ToggleButton>
+            <ToggleButton value="right" aria-label="right aligned">
+              <Typography
+                variant="h7"
+                noWrap
+                component="div"
+                sx={{ display: { xs: 'none', sm: 'block' } }}
+                style={{color: 'white', letterSpacing: 3}}
+              >
+                Events
+              </Typography>
+            </ToggleButton>
+            <ToggleButton value="justify" aria-label="justified">
+              <Typography
+                variant="h7"
+                noWrap
+                component="div"
+                sx={{ display: { xs: 'none', sm: 'block' } }}
+                style={{color: 'white', letterSpacing: 3}}
+              >
+                Contact
+              </Typography>
+            </ToggleButton>
+          </ToggleButtonGroup>
+
+            {/* <Button 
+              variant='text' 
+              style={{color: 'white', letterSpacing: 2}} 
+              onClick={btnColor => setBtnColor(btnColor)}
+            >
+              Home
+            </Button>
+            <Button 
+              variant='text' 
+              style={{color: 'white', letterSpacing: 2}}
+            >
+              The Team
+            </Button>
+            <Button 
+              variant='text' 
+              style={{color: 'white', letterSpacing: 2}}
+            >
+              Events
+            </Button>
+            <Button 
+              variant='text' 
+              style={{color: 'white', letterSpacing: 2}}
+            >
+              Contact
+            </Button> */}
           </Box>
         </Toolbar>
       </AppBar>
